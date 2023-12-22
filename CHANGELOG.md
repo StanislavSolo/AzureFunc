916856225a3b9e7a5894cb44634e0f295964f39c
Stanislav Solodukhin <Stanislav.Solodukhin@cantire.com>
2023-12-22 14:22:50 +0200

    added using App Configuration

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [1.0.2] - 2023-12-22
+
+-   Used App Configuration
+
 ## [1.0.1] - 2023-12-20
 
 -   Added azure functions
--- http-get-product-list/index.ts
+++ http-get-product-list/index.ts
@@ -1,15 +1,21 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import ProductService from '../common/product.service';
+import { getConfigSettings } from '../common/config.service';
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
+const httpTrigger: AzureFunction = async function (
+    context: Context,
+    req: HttpRequest
+): Promise<void> {
     try {
         const productService = new ProductService();
         const product = await productService.getProductList();
+        const settings = await getConfigSettings('testKey');
 
         context.res = {
             status: 200,
             headers: {
                 'content-type': 'application/json',
+                testKey: settings,
             },
             body: product,
         };
