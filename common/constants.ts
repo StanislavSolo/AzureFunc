3c13efa45ba9a7f7236e13b004c90d0c294a962c
Stanislav Solodukhin <Stanislav.Solodukhin@cantire.com>
2024-02-05 13:06:24 +0200

    implemented service bus

--- common/constants.ts
+++ common/constants.ts
@@ -1,3 +1,4 @@
 export const TOTAL_PRODUCTS = 'total';
 export const BLOB_CONTAINER_NAME = 'uploaded';
 export const PARSE_CONTAINER_NAME = 'parsed';
+export const SERVICE_BUS_QUEUE_NAME = 'solodukhin_queue';
